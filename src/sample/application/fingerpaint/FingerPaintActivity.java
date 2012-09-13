e69728e03e769497718341ccdf9420b31c20dc94
takamal <crossroads22@auone.jp>
2012-09-13 22:20:45 +0900

    add2

--- src/sample/application/fingerpaint/FingerPaintActivity.java
+++ src/sample/application/fingerpaint/FingerPaintActivity.java
@@ -1,9 +1,12 @@
 package sample.application.fingerpaint;
 
+import java.security.PublicKey;
+
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.view.MotionEvent;
@@ -14,8 +17,10 @@ public class FingerPaintActivity extends Activity implements OnTouchListener{
 	public Paint paint;
 	public Path path;
 	public Bitmap bitmap;
-	public Float x1, y1;
-	
+	public Float x1;
+	public Float y1;
+	public Integer w;
+	public Integer h;
 	
 //	抽象メソッドをオーバーライドしていないのでエラーがでる。
 	public boolean onTouch (View v, MotionEvent event){
